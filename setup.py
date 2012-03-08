f2d53aafb17d1b9faf3f8404f1d1d440627b799c
guijarro <guijarro@linguijarro.esrf.fr>
2012-03-08 13:40:34 +0100

    same directory structure + setup.py file as master branch

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+import sys
+
+setup(name = "SpecClient",version = "1.0",
+      description = "Python package for communicating with spec", 
+      author="Matias Guijarro, BCU(Bliss), ESRF",
+      package_dir={"SpecClient": "SpecClient"},
+      packages = ["SpecClient"])
+

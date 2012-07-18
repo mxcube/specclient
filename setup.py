55fe348c632edd845e9b150e6e6d8a662fad7367
guijarro <guijarro@linguijarro.esrf.fr>
2012-07-18 09:16:24 +0200

    installs in a SpecClient_gevent directory, not to confuse with SpecClient (without gevent)

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import sys
 setup(name = "SpecClient",version = "1.0",
       description = "Python package for communicating with spec", 
       author="Matias Guijarro, BCU(Bliss), ESRF",
-      package_dir={"SpecClient": "SpecClient"},
-      packages = ["SpecClient"])
+      package_dir={"SpecClient_gevent": "SpecClient"},
+      packages = ["SpecClient_gevent"])
 
